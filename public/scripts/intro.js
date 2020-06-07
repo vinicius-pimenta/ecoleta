6d7a2886ad0905ff9550493c7a73389d9fe1e658
vinicius-pimenta <viniciustavarespimenta.2020@gmail.com>
2020-06-07 16:23:56 -0300

    :rocket: Add Ecole project

---
+++ public/scripts/intro.js
@@ -0,0 +1,77 @@
+// comments
+// document.write("Hello")
+
+// Variáveis, tipos de dados
+ 
+// String
+// const s1 = "Isto é uma String"
+// const s2 = 'Isto também é uma String'
+// const s3 = `Isto também é uma String`
+
+// number
+// const n1 = 1
+// const n2 = 12
+// document.write(n1 + n2)
+
+// boolean - true ou false
+// const bTrue = true
+// const bFalse = false
+// document.write(bFalse)
+
+// objeto possuem propriedades e funcionalidades
+// const pessoa = {
+//   altura: "1,80m",
+//   idade: 24,
+//   solteiro: true,
+//   correr(){
+//     document.write("executar grande logica de correr")
+//   }
+// }
+
+// pessoa.correr()
+
+// Array - Vetores
+// const colecaoDeBolinhas = ["blue", "green", 1, {name:"My Name"}]
+
+// document.write(colecaoDeBolinhas[3].name)
+
+// Funcoes - Funcionalidades - Atalhos - Reuso de código
+
+// Função já registrada
+// function sayMyName() {
+//   document.write("Edson")
+// }
+
+// Função sendo executada
+// sayMyName()
+
+// Funcão Registrada
+// function sayMyName(name) {
+//     document.write(name)
+//   }
+  
+//   sayMyName("Edson")
+//   sayMyName(' Enricco')
+//   sayMyName(` Rose`)
+  
+// Condicional
+
+// const notaFinal = 4
+
+// if(notaFinal < 5){
+//    document.write("Reprovado")
+//    } else {
+//   document.write("Aprovado")
+// }
+
+// loop repetições
+
+// for (i = 0; i < 10; i++) {
+//   document.write("<p>Alo</p>")
+// }
+
+// for (i = 0; i < 10; i++) {
+//   document.write(`<p>${i}</p>`)
+// }
+  
+  
